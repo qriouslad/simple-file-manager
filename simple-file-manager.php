4a7bd2f69b8163a6619b08ea0e3c0324c325815b
Bowo <wibowosulistio@gmail.com>
2022-04-16 12:52:17 +0700

    v1.2.0 release

--- simple-file-manager.php
+++ simple-file-manager.php
@@ -15,8 +15,8 @@
  * @wordpress-plugin
  * Plugin Name:       Simple File Manager
  * Plugin URI:        https://wordpress.org/plugins/simple-file-manager/
- * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
- * Version:           1.1.0
+ * Description:       Lightweight file manager focusing as a code viewer and editor with syntax highlighting.
+ * Version:           1.2.0
  * Author:            Bowo
  * Author URI:        https://bowo.io
  * License:           GPL-2.0+
@@ -35,7 +35,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'SIMPLE_FILE_MANAGER_VERSION', '1.1.0' );
+define( 'SIMPLE_FILE_MANAGER_VERSION', '1.2.0' );
 
 /**
  * The code that runs during plugin activation.
